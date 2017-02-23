0e53fcef7b407b45431e5220c16a3a31cf7b020c
d-apurva <apurva.dhoble@click2cloud.net>
2017-02-23 14:34:18 +0530

    Update run.bat

--- UAT Automation_TFS -DEMO_Final/run.bat
+++ UAT Automation_TFS -DEMO_Final/run.bat
@@ -1,4 +1,4 @@
-set projectpath=E:\sapan_sir_automation_demo\UAT Automation_TFS -DEMO_Final
+set projectpath=C:\Users\apurva.dhoble\Application\UAT Automation_TFS -DEMO_Final
 cd %projectpath%
 set classpath=%projectpath%\bin;%projectpath%\NewJars\*;%projectpath%\All_Jars\*;
 java org.testng.TestNG %projectpath\%testng.xml
